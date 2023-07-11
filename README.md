1be6a212547df3f5ea04780b953bdf1f6bf129f0
Anthony Kiplimo <anthonylimo90@gmail.com>
2023-07-11 12:46:39 +0300

    Initial working commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# OpenAI Chat System
+This is a NodeJS application that allows you to fetch text responses from the OpenAI text-davinci-300 model to any questions that are passed into the `/ask` endpoint. 
+
+## Running this code
+To run this code, `git clone` this repository. Run `npm install` then `node index.js`.
+
+#### More
+This code base was inspired by this [article](https://medium.com/bitsrc/interacting-with-openai-in-node-js-and-express-647e771fc4ad)
---
+++ index.js
@@ -0,0 +1,55 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+const express = require("express");
+
+const pug = require("pug");
+
+const app = express();
+app.use(express.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = process.env.PORT || 3000;
+
+// Add in view for users to submit their prompts
+
+app.get("/", (req, res) => {
+    res.render(
+        "index",
+        { title: "Pug with Express", message: "Hello from Limo" }
+    );
+});
+
+app.post("/ask", async (req, res) => {
+    const prompt = req.body.prompt;
+
+    try {
+        if (prompt == null) {
+            throw new Error("Uh oh! No prompt was provided");
+        }
+        const response = await openai.createCompletion({
+            model: "text-davinci-300",
+            prompt
+        });
+        const completion = response.data.choices[0].text;
+        return res.status(200).json({
+            success: true,
+            message: prompt
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
